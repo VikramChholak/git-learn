72877930c2a69c68f9c4d8fe564005143f631f36
Buddhi Prakash <bp@chholak.com>
2022-01-15 18:56:10 +0530

    main.c file updated.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 
 void main(){
-    printf("Temp");
+    int a=0,b=0;
+    
+    // Take input from the user.
+    printf("Enter Num0: ");
+    scanf("%d",&a);
+    printf("Enter Num1: ");
+    scanf("%d",&b);
+    
+    // Print result of adder function
+    printf("Result: %d",adder(a,b));
+    printf("\n");
+}
+
+int adder(int Num0,int Num1) {
+    return Num0+Num1;
 }
